e7167e73c0af90cb98c21231d053e82d6b467b42
Tom Randolph <tom@snowcapdigital.com>
2015-08-16 01:21:30 -0600

    Add watcher

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,5 +54,14 @@ module.exports = function( grunt ){
     ] );
 
     /** This is what will run if you don't give Grunt any directions **/
-    grunt.registerTask( 'default', [ 'build' ] );
+    grunt.registerTask( 'default', function(){
+        var watch = grunt.option( 'watch' );
+        var tasks = [ 'build' ];
+
+        if( watch ){
+            tasks.push( 'watch' );
+        }
+
+        grunt.task.run( tasks );
+    } );
 };
